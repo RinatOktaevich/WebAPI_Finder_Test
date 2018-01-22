55a2ef8ceda606cd1c5fe72899442f98b5a8c4bd
hell_of_god <rinatoktaevich@gmail.com>
2018-01-22 15:01:38 +0200

    First Commit

---
+++ WebAPI_Finder_Test/Migrations/Configuration.cs
@@ -0,0 +1,39 @@
+namespace WebAPI_Finder_Test.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebAPI_Finder_Test.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebAPI_Finder_Test.Models.ApplicationDbContext context)
+        {
+
+           
+            ApplicationUser user = new ApplicationUser() { Email = "raot@gmail.ru", UserName = "raot@gmail.ru", PasswordHash="111111111" };
+            context.Users.Add(user);
+
+
+
+                //  This method will be called after migrating to the latest version.
+
+                //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+                //  to avoid creating duplicate seed data. E.g.
+                //
+                //    context.People.AddOrUpdate(
+                //      p => p.FullName,
+                //      new Person { FullName = "Andrew Peters" },
+                //      new Person { FullName = "Brice Lambson" },
+                //      new Person { FullName = "Rowan Miller" }
+                //    );
+                //
+            }
+    }
+}
---
+++ WebAPI_Finder_Test/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAPI_Finder_Test.Startup))]
+
+namespace WebAPI_Finder_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
