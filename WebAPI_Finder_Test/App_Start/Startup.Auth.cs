65f6cc2db7b5a24b044a730531030d89382db351
hell_of_god <rinatoktaevich@gmail.com>
2018-01-23 23:55:51 +0200

    add  CorsOptions.Allow all

--- WebAPI_Finder_Test/App_Start/Startup.Auth.cs
+++ WebAPI_Finder_Test/App_Start/Startup.Auth.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security.OAuth;
 using Owin;
 using WebAPI_Finder_Test.Providers;
 using WebAPI_Finder_Test.Models;
+using Microsoft.Owin.Cors;
 
 namespace WebAPI_Finder_Test
 {
@@ -22,6 +23,8 @@ namespace WebAPI_Finder_Test
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            app.UseCors(CorsOptions.AllowAll);
+
             // Configure the db context and user manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
