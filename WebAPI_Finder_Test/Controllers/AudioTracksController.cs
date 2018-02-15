f3ff747cd72fb97bc4fba733d858a5d5458212ba
hell_of_god <rinatoktaevich@gmail.com>
2018-02-15 23:06:16 +0200

    Fix bug validation

--- WebAPI_Finder_Test/Controllers/AudioTracksController.cs
+++ WebAPI_Finder_Test/Controllers/AudioTracksController.cs
@@ -41,7 +41,7 @@ namespace WebAPI_Finder_Test.Controllers
             }
 
             ApplicationDbContext db = new ApplicationDbContext();
-            var user = db.Users.First(u => u.UserName == email);//.AudioTracks.Add(new Audio(_url: soundtrack, _pr: performer, _ttl: tittle));
+            var user = db.Users.First(u => u.UserName == email);
 
             user.AudioTracks.Add(new Audio(_url: soundtrack, _pr: performer, _ttl: tittle,authLogin:user.Login));
 
