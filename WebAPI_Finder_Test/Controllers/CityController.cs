4cef82f2ce6347b29d2f948793757c3d6e152d98
hell_of_god <rinatoktaevich@gmail.com>
2018-02-24 00:08:17 +0200

    Made controllers for 'City' and 'Country' entyties

---
+++ WebAPI_Finder_Test/Controllers/CityController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebAPI_Finder_Test.Models;
+
+namespace WebAPI_Finder_Test.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/City")]
+    public class CityController : ApiController
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<HttpResponseMessage> AddCity(int countryid, string name)
+        {
+            var country = db.Countries.Find(countryid);
+            if (country == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Country Id doesn`t exist");
+            }
+
+            db.Cities.Add(new City() { CountryId = countryid, Name = name });
+
+            await db.SaveChangesAsync();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<HttpResponseMessage> DeleteCity(int cityid)
+        {
+            var city = db.Cities.Find(cityid);
+            if (city == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "City Id doesn`t exist");
+            }
+
+            db.Cities.Remove(city);
+
+            await db.SaveChangesAsync();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+
+        [HttpPost]
+        [Route("Rename")]
+        public async Task<HttpResponseMessage> RenameCity(int cityid, string name)
+        {
+            var city = db.Cities.Find(cityid);
+            if (city == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "City Id doesn`t exist");
+            }
+
+            city.Name = name;
+
+            db.Entry(city).State = EntityState.Modified;
+
+            await db.SaveChangesAsync();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+
+
+        [HttpGet]
+        [Route("ToList")]
+        public async Task<IHttpActionResult> GetCities()
+        {
+            var cities = await db.Cities.Include("Country").ToListAsync();
+            return Ok(cities); ;
+        }
+
+    }
+}
--- WebAPI_Finder_Test/Controllers/CountryController.cs
+++ WebAPI_Finder_Test/Controllers/CountryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -17,13 +18,58 @@ namespace WebAPI_Finder_Test.Controllers
 
         [HttpPost]
         [Route("Add")]
-        public async  Task<HttpResponseMessage> AddCountry(string name)
+        public async Task<HttpResponseMessage> AddCountry(string name)
         {
             db.Countries.Add(new Country() { Name = name });
             await db.SaveChangesAsync();
 
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<HttpResponseMessage> DeleteCountry(int idcountry)
+        {
+            var country = db.Countries.Find(idcountry);
+            if (country == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            db.Countries.Remove(country);
+            await db.SaveChangesAsync();
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+
+
+        [HttpPost]
+        [Route("Rename")]
+        public async Task<HttpResponseMessage> RenameCountry(int idcountry, string name)
+        {
+            var country = db.Countries.Find(idcountry);
+            if (country == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            country.Name = name;
+            db.Entry(country).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+
+        [HttpGet]
+        [Route("ToList")]
+        public IHttpActionResult Get_Countries()
+        {
+            var res = db.Countries.ToList();
+            return Ok(res);
+        }
+
 
     }
 }
