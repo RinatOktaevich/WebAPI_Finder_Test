fa543850fce46a13c0c3293a96e841023476d354
hell_of_god <rinatoktaevich@gmail.com>
2018-03-06 23:01:47 +0200

    Soc by pattern 1.0

--- WebAPI_Finder_Test/Controllers/SocialNetworksController.cs
+++ WebAPI_Finder_Test/Controllers/SocialNetworksController.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Http;
 using WebAPI_Finder_Test.Models;
+using WebAPI_Finder_Test.Models.SocialLinks;
 
 namespace WebAPI_Finder_Test.Controllers
 {
@@ -14,150 +15,158 @@ namespace WebAPI_Finder_Test.Controllers
     [RoutePrefix("api/SocialNetworks")]
     public class SocialNetworksController : ApiController
     {
-        ApplicationDbContext db = new ApplicationDbContext();
         /// <summary>
         /// Здесь для того чтобы установить значение нужно просто передать нужный параметр,не обязательно передавать все параметры.
         //Но что бы удалить один из параметров нужно передать значение 'null'
         /// </summary>
-        [HttpPost]
-        [Route("Set")]
-        public async Task<HttpResponseMessage> SetUrl(string iduser, string insta = "", string vk = "", string fcBook = "", string youtb = "", string twitter = "")
-        {
-            var user = db.Users.Find(iduser);
+        //[HttpPost]
+        //[Route("Set")]
+        //public async Task<HttpResponseMessage> SetUrl(string iduser, string insta = "", string vk = "", string fcBook = "", string youtb = "", string twitter = "")
+       // {
+            //var user = db.Users.Find(iduser);
 
-            if (user == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, "User doesn`t exict");
-            }
+            //if (user == null)
+            //{
+            //    return Request.CreateResponse(HttpStatusCode.BadRequest, "User doesn`t exict");
+            //}
 
-            var soc = user.SocNetworks;
+            //var soc = user.SocNetworks;
 
-            if (user.SocNetworks == null)
-            {
-                soc = new SocialNetworks();
-            }
+            //if (user.SocNetworks == null)
+            //{
+            //    soc = new SocialNetworks();
+            //}
 
             //Instargram
-            if (insta != "")
-            {
-                if (insta != "null")
-                {
-                    Regex reg = new Regex(@"https://www.instagram.com/.{3,}");
-                    if (reg.IsMatch(insta))
-                    {
-                        soc.Instagram = insta;
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Instagram Url isn`t valid");
-                    }
-                }
-                else
-                {
-                    soc.Instagram = null;
-                }
-            }
+            //if (insta != "")
+            //{
+            //    if (insta != "null")
+            //    {
+            //        Regex reg = new Regex(@"https://www.instagram.com/.{3,}");
+            //        if (reg.IsMatch(insta))
+            //        {
+            //            soc.Instagram = insta;
+            //        }
+            //        else
+            //        {
+            //            return Request.CreateResponse(HttpStatusCode.InternalServerError, "Instagram Url isn`t valid");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        soc.Instagram = null;
+            //    }
+            //}
 
 
             //Vk
-            if (vk != "")
-            {
-                if (vk != "null")
-                {
-                    Regex reg = new Regex(@"https://vk.com/.{3,}");
-                    if (reg.IsMatch(vk))
-                    {
-                        soc.Vk = vk;
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Vk Url isn`t valid");
-                    }
-                }
-                else
-                {
-                    soc.Vk = null;
-                }
-            }
+            //if (vk != "")
+            //{
+            //    if (vk != "null")
+            //    {
+            //        Regex reg = new Regex(@"https://vk.com/.{3,}");
+            //        if (reg.IsMatch(vk))
+            //        {
+            //            soc.Vk = vk;
+            //        }
+            //        else
+            //        {
+            //            return Request.CreateResponse(HttpStatusCode.InternalServerError, "Vk Url isn`t valid");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        soc.Vk = null;
+            //    }
+            //}
 
 
 
             //facebook
-            if (fcBook != "")
-            {
-                if (fcBook != "null")
-                {
-                    Regex reg = new Regex(@"https://www.facebook.com/.{3,}");
-                    if (reg.IsMatch(fcBook))
-                    {
-                        soc.FaceBook = fcBook;
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "facebook Url isn`t valid");
-                    }
-                }
-                else
-                {
-                    soc.FaceBook = null;
-                }
-            }
+            //if (fcBook != "")
+            //{
+            //    if (fcBook != "null")
+            //    {
+            //        Regex reg = new Regex(@"https://www.facebook.com/.{3,}");
+            //        if (reg.IsMatch(fcBook))
+            //        {
+            //            soc.FaceBook = fcBook;
+            //        }
+            //        else
+            //        {
+            //            return Request.CreateResponse(HttpStatusCode.InternalServerError, "facebook Url isn`t valid");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        soc.FaceBook = null;
+            //    }
+            //}
 
 
 
             //YouTube
-            if (youtb != "")
-            {
-                if (youtb != "null")
-                {
-                    Regex reg = new Regex(@"https://www.youtube.com/channel/.{3,}");
-                    if (reg.IsMatch(youtb))
-                    {
-                        soc.YouTube = youtb;
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "YouTube Url isn`t valid");
-                    }
-                }
-                else
-                {
-                    soc.YouTube = null;
-                }
-            }
+            //if (youtb != "")
+            //{
+            //    if (youtb != "null")
+            //    {
+            //        Regex reg = new Regex(@"https://www.youtube.com/channel/.{3,}");
+            //        if (reg.IsMatch(youtb))
+            //        {
+            //            soc.YouTube = youtb;
+            //        }
+            //        else
+            //        {
+            //            return Request.CreateResponse(HttpStatusCode.InternalServerError, "YouTube Url isn`t valid");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        soc.YouTube = null;
+            //    }
+            //}
 
 
 
             //Twitter
-            if (twitter != "")
-            {
-                if (twitter != "null")
-                {
-                    Regex reg = new Regex(@"https://twitter.com/.{3,}");
-                    if (reg.IsMatch(twitter))
-                    {
-                        soc.Twitter = twitter;
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.InternalServerError, "Twitter Url isn`t valid");
-                    }
-                }
-                else
-                {
-                    soc.Twitter = null;
-                }
-            }
+            //if (twitter != "")
+            //{
+            //    if (twitter != "null")
+            //    {
+            //        Regex reg = new Regex(@"https://twitter.com/.{3,}");
+            //        if (reg.IsMatch(twitter))
+            //        {
+            //            soc.Twitter = twitter;
+            //        }
+            //        else
+            //        {
+            //            return Request.CreateResponse(HttpStatusCode.InternalServerError, "Twitter Url isn`t valid");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        soc.Twitter = null;
+            //    }
+            //}
 
-            user.SocNetworks = soc;
+            //user.SocNetworks = soc;
 
-            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            //db.Entry(user).State = System.Data.Entity.EntityState.Modified;
 
-            await db.SaveChangesAsync();
+            //await db.SaveChangesAsync();
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            //return new HttpResponseMessage(HttpStatusCode.OK);
+       // }
+
+
+
+        [HttpPost]
+        [Route("Set/Instagram")]
+        public async Task<HttpResponseMessage> Insta(string iduser,string value)
+        {
+            InstagramLink insta = new InstagramLink(Request);
+            return await insta.Algorythm(iduser, value);
         }
-
 
     }
 }
---
+++ WebAPI_Finder_Test/Models/SocialLinks/SocialNet.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPI_Finder_Test.Models.SocialLinks
+{
+    public abstract class SocialNet
+    {
+        ApplicationUser user;
+        ApplicationDbContext db = new ApplicationDbContext();
+        HttpResponseMessage Response;
+
+        protected HttpRequestMessage Request;
+        protected SocialNetworks soc;
+
+        public SocialNet(HttpRequestMessage _request)
+        {
+            Request = _request;
+        }
+
+        public async Task<HttpResponseMessage> Algorythm(string iduser, string value)
+        {
+            var resp = Checks(iduser);
+            if (resp.StatusCode != HttpStatusCode.OK)
+                return resp;
+
+            resp = SetValue(value);
+            if (resp.StatusCode != HttpStatusCode.OK)
+                return resp;
+
+
+            return await SaveChanges();
+
+        }
+
+        //Предварительная проверка на различные данные
+        public HttpResponseMessage Checks(string iduser)
+        {
+            user = db.Users.Find(iduser);
+
+            if (user == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Code = 1, Message = "you asshole" });  // Request.CreateResponse(HttpStatusCode.BadRequest, "User doesn`t exict");
+            }
+
+            soc = user.SocNetworks;
+
+            if (user.SocNetworks == null)
+            {
+                soc = new SocialNetworks();
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        //Сохранение результата
+        public async Task<HttpResponseMessage> SaveChanges()
+        {
+            user.SocNetworks = soc;
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            await db.SaveChangesAsync();
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        public abstract HttpResponseMessage SetValue(string value);
+
+    }
+
+
+    public class InstagramLink : SocialNet
+    {
+        public InstagramLink(HttpRequestMessage _request) : base(_request)
+        {
+        }
+
+        public override HttpResponseMessage SetValue(string value)
+        {
+            if (value != "")
+            {
+                Regex reg = new Regex(@"https://www.instagram.com/.{3,}");
+                if (reg.IsMatch(value))
+                {
+                    soc.Instagram = value;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Instagram Url isn`t valid");
+                }
+            }
+            else
+            {
+                soc.Instagram = null;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+
+        }
+    }
+
+    public class VkLink : SocialNet
+    {
+        public VkLink(HttpRequestMessage _request) : base(_request)
+        {
+        }
+
+        public override HttpResponseMessage SetValue(string value)
+        {
+            if (value != "")
+            {
+                Regex reg = new Regex(@"https://vk.com/.{3,}");
+                if (reg.IsMatch(value))
+                {
+                    soc.Vk = value;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Vk Url isn`t valid");
+                }
+            }
+            else
+            {
+                soc.Vk = null;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+
+    public class facebookLink : SocialNet
+    {
+        public facebookLink(HttpRequestMessage _request) : base(_request)
+        {
+        }
+
+        public override HttpResponseMessage SetValue(string value)
+        {
+            if (value != "")
+            {
+                Regex reg = new Regex(@"https://www.facebook.com/.{3,}");
+                if (reg.IsMatch(value))
+                {
+                    soc.FaceBook = value;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "facebook Url isn`t valid");
+                }
+            }
+            else
+            {
+                soc.FaceBook = null;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+
+        }
+    }
+
+    public class YouTubeLink : SocialNet
+    {
+        public YouTubeLink(HttpRequestMessage _request) : base(_request)
+        {
+        }
+
+        public override HttpResponseMessage SetValue(string value)
+        {
+            if (value != "")
+            {
+                Regex reg = new Regex(@"https://www.youtube.com/channel/.{3,}");
+                if (reg.IsMatch(value))
+                {
+                    soc.YouTube = value;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "YouTube Url isn`t valid");
+                }
+            }
+            else
+            {
+                soc.YouTube = null;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+
+    public class Twitter : SocialNet
+    {
+        public Twitter(HttpRequestMessage _request) : base(_request)
+        {
+        }
+
+        public override HttpResponseMessage SetValue(string value)
+        {
+            if (value != "")
+            {
+                Regex reg = new Regex(@"https://twitter.com/.{3,}");
+                if (reg.IsMatch(value))
+                {
+                    soc.Twitter = value;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Twitter Url isn`t valid");
+                }
+            }
+            else
+            {
+                soc.Twitter = null;
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+    }
+
+
+}
